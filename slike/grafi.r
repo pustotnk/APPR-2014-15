38f563c91038310330aeec0e4ee1257403b971fc
Tomaz Pustotnik <pustexxx@gmail.com>
2015-01-12 12:15:01 +0100

    grafu sem dodal imena

--- slike/grafi.r
+++ slike/grafi.r
@@ -1,5 +1,9 @@
 pdf("slike/grafi1.pdf",paper="a4")
-barplot(NOGOMETASI[NOGOMETASI$Goals>100,'Goals'],xlab="IGRALEC", ylab="ŠTEVILO ZADETKOV", main= 'ŠTEVILO ZADETKOV IGRALCEV, KI SO ZADELI VEČ KOT 100-KRAT', las=2, cex.names=0.75, col="blue")
-pdf("slike/grafi1.pdf",paper="a4")
-barplot(NOGOMETASI[NOGOMETASI$Appearances>50,'Appearances'],xlab="IGRALEC", ylab="ŠTEVILO NASTOPOV", main= 'ŠTEVILO NASTOPOV IGRALCEV, KI SO ZA KLUB NASTOPILI VEČ KOT 50 KRAT', las=2, cex.names=0.75, col="blue")
+a<-rownames(NOGOMETASI)
+barplot(NOGOMETASI[NOGOMETASI$Goals>100,'Goals'],names.arg = a[NOGOMETASI$Goals>100],xlab="IGRALEC", ylab="ŠTEVILO ZADETKOV", main= 'ŠTEVILO ZADETKOV IGRALCEV, KI SO ZADELI VEČ KOT 100-KRAT', las=2, cex.names=0.75, col="blue")
+dev.off()
 
+pdf("slike/grafi2.pdf",paper="a4")
+a<-rownames(NOGOMETASI)
+barplot(NOGOMETASI[NOGOMETASI$Appearances>50,'Appearances'],names.arg = a[NOGOMETASI$Appearances>50],xlab="IGRALEC", ylab="ŠTEVILO NASTOPOV", main= 'ŠTEVILO NASTOPOV IGRALCEV, KI SO ZA KLUB NASTOPILI VEČ KOT 50 KRAT', las=2, cex.names=0.75, col="blue")
+dev.off()
